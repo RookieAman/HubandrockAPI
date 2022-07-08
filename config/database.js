6bea1f9ff969c32e6b8f23e734079e6dd49309f2
RookieAman <107254599+RookieAman@users.noreply.github.com>
2022-07-08 16:16:00 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ var pool_cenobi_min = mysql.createPool({
         host: 'localhost',
         port: '3306',
         user: 'root',
-        password: '',
-        database: 'hubandrock',
+        password: 'V#f,7F6c%f@9',
+        database: 'hubnrock',
         debug: 'false',
         dateStrings: true,
         multipleStatements: true
