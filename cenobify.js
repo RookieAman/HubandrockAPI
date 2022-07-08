006fc53ea9f10bddee8d1d2a1cdf27211503d58e
RookieAman <107254599+RookieAman@users.noreply.github.com>
2022-07-09 00:48:03 +0200

    Update cenobify.js

--- cenobify.js
+++ cenobify.js
@@ -31,6 +31,22 @@ var server = https.createServer(
   app
 );
 
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  res.header("Content-Type", "application/json");
+
+  if (req.method == "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
